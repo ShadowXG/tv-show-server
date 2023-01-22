ff9daf0191094d4cfddf76f999a7621fdeba70dd
Travis <travishersh0@gmail.com>
2023-01-22 18:08:37 -0500

    set up a tv controller and connected it to the server

---
+++ controllers/tv-showsControllers.js
@@ -0,0 +1,107 @@
+///////////////////////////////
+//// Import Dependencies   ////
+///////////////////////////////
+const express = require('express')
+const TV = require('../models/tv-shows')
+
+//////////////////////////
+//// Create Router    ////
+//////////////////////////
+const router = express.Router()
+
+//////////////////////
+//// Routes       ////
+//////////////////////
+
+// The seed route
+router.get('/seed', (req, res) => {
+    // array of starter(tv shows)
+    const startTvShows = [
+        {name: 'Psych', genre: 'crime, comedy', inProduction: false},
+        {name: 'Suits', genre: 'drama', inProduction: false},
+        {name: 'Doctor Who', genre: 'adventure', inProduction: true},
+        {name: 'Brooklyn Nine-Nine', genre: 'crime, comedy', inProduction: false},
+    ]
+    // delet all the tv shows in the db
+    TV.deleteMany({})
+        .then(() => {
+            // create the start tv shows
+            TV.create(startTvShows)
+                // tell our db what to do with success and failures
+                .then(data => {
+                    res.json(data)
+                })
+                .catch(err => console.log('The following error occurred: \n', err))
+        })
+})
+
+// INDEX route
+// Read -> finds and displays all tv shows
+router.get('/', (req, res) => {
+    // find all the tv shows
+    TV.find({})
+        // send json if successful
+        .then(tvs => { res.json({ tvs: tvs })})
+        // catch errors if they occur
+        .catch(err => console.log('The following error occurred: \n', err))
+})
+
+// CREATE route
+// Create -> receives a request, and creates a new document
+router.post('/', (req, res) => {
+    const newTvShow = req.body
+    TV.create(newTvShow)
+        .then(tv => {
+            // sending a 201 status, along with the new tv show
+            res.status(201).json({ tv: tv.toObject() })
+        })
+        // send an err if one occurs
+        .catch(err => console.log('The following error occurred: \n', err))
+})
+
+// PUT route
+// Update -> updates a specific tv show
+router.put('/:id', (req, res) => {
+    // save the id for easy reference later
+    const id = req.params.id
+    // save the req.body for easy reference later
+    const updatedTvShow = req.body
+    // find the tv show by id and update it
+    TV.findByIdAndUpdate(id, updatedTvShow, { new: true })
+        .then(tv => {
+            console.log('The updated tv show: ', tv)
+            res.sendStatus(204)
+        })
+        .catch(err => console.log('The following error occurred: \n', err))
+})
+
+// DELETE route
+// Delete -> destroys a single tv show
+router.delete('/:id', (req, res) => {
+    // get the id
+    const id = req.params.id
+    // find and delete the tv show
+    TV.findByIdAndRemove(id)
+        .then(() => {
+            res.sendStatus(204)
+        })
+        .catch(err => console.log('The following error occurred: \n', err))
+})
+
+// SHOW route
+// read -> finds and displays a single resource
+router.get('/:id', (req, res) => {
+    // get the id
+    const id = req.params.id
+    TV.findById(id)
+        .then(tv => {
+            // send the tv show as json upon success
+            res.json({ tv: tv })
+        })
+        .catch(err => console.log('The following error occurred: \n', err))
+})
+
+//////////////////////////
+//// Export Router    ////
+//////////////////////////
+module.exports = router
--- server.js
+++ server.js
@@ -6,11 +6,7 @@ const express = require("express") // import express
 const morgan = require("morgan") // import morgan
 const mongoose = require("mongoose") // import mongoose
 const path = require("path") // import path module
-
-///////////////////////////////
-//// Import Our Models     ////
-///////////////////////////////
-const TV = require('./models/tv-shows')
+const TvRouter = require('./controllers/tv-showsControllers')
 
 ////////////////////////////////
 //// Database Connection    ////
@@ -51,80 +47,8 @@ app.get('/', (req, res) => {
     res.send('Server is live, ready for requests')
 })
 
-// The seed route
-app.get('/tv/seed', (req, res) => {
-    // array of starter(tv shows)
-    const startTvShows = [
-        {name: 'Psych', genre: 'crime, comedy', inProduction: false},
-        {name: 'Suits', genre: 'drama', inProduction: false},
-        {name: 'Doctor Who', genre: 'adventure', inProduction: true},
-        {name: 'Brooklyn Nine-Nine', genre: 'crime, comedy', inProduction: false},
-    ]
-    // delete all the tv shows in the db
-    TV.deleteMany({})
-        .then(() => {
-            // create the start tv shows
-            TV.create(startTvShows)
-                // tell our db what to do with success and failures
-                .then(data => {
-                    res.json(data)
-                })
-                .catch(err => console.log('The following error occurred: \n', err))
-        })
-})
-
-// INDEX route
-// Read -> finds and displays all tv shows
-app.get('/tv', (req, res) => {
-    // find all the tv shows
-    TV.find({})
-        // send json if successful
-        .then(tvs => { res.json({ tvs: tvs })})
-        // catch errors if they occur
-        .catch(err => console.log('The following error occurred: \n', err))
-})
-
-// CREATE route
-// Create -> receives a request, and creates a new document
-app.post('/tv', (req, res) => {
-    const newTvShow = req.body
-    TV.create(newTvShow)
-        .then(tv => {
-            // sending a 201 status, along with the new tv show
-            res.status(201).json({ tv: tv.toObject() })
-        })
-        // send an err if one occurs
-        .catch(err => console.log('The following error occurred: \n', err))
-})
-
-// PUT route
-// Update -> updates a specific tv show
-app.put('/tv/:id', (req, res) => {
-    // save the id for easy reference later
-    const id = req.params.id
-    // save the req.body for easy reference later
-    const updatedTvShow = req.body
-    // find the tv show by id and update it
-    TV.findByIdAndUpdate(id, updatedTvShow, { new: true })
-        .then(tv => {
-            console.log('The updated tv show: ', tv)
-            res.sendStatus(204)
-        })
-        .catch(err => console.log('The following error occurred: \n', err))
-})
-
-// DELETE route
-// Delete -> destroys a single tv show
-app.delete('/tv/:id', (req, res) => {
-    // get the id
-    const id = req.params.id
-    // find and delete the tv show
-    TV.findByIdAndRemove(id)
-        .then(() => {
-            res.sendStatus(204)
-        })
-        .catch(err => console.log('The following error occurred: \n', err))
-})
+// register the routes
+app.use('/tv', TvRouter)
 
 ///////////////////////////
 //// Server Listener   ////

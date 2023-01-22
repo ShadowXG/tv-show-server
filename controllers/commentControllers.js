7a63a26ca43bc08cf5034b4e142ef312e544a5b9
Travis <travishersh0@gmail.com>
2023-01-22 18:57:54 -0500

    users can now delete comments

--- controllers/commentControllers.js
+++ controllers/commentControllers.js
@@ -19,25 +19,53 @@ router.post('/:tvId', (req,res) => {
     const tvId = req.params.tvId
     if (req.session.loggedIn) {
         req.body.author = req.session.userId
+        const theComment = req.body
+        // find a specific tv show
+        TV.findById(tvId)
+            .then(tv => {
+                tv.comments.push(theComment)
+                // save the tv
+                return tv.save()
+            })
+            .then(tv => {
+                // respond with a 201 and the tv itself
+                res.status(201).json({ tv: tv })
+            })
+            .catch(err => {
+                // catch any errors
+                console.log(err)
+                res.status(400).json(err)
+            })
     } else {
         res.sendStatus(401)
     }
-    const theComment = req.body
-    // find a specific tv show
+})
+
+// DELETE route
+// only the author of the comment can delete the comment
+router.delete('/delete/:tvId/:commId', (req, res) => {
+    // isolate the id and save them to variables for easy access
+    const { tvId, commId } = req.params
+    // get the tv show
     TV.findById(tvId)
         .then(tv => {
-            tv.comments.push(theComment)
-            // save the tv
-            return tv.save()
-        })
-        .then(tv => {
-            // respond with a 201 and the tv itself
-            res.status(201).json({ tv: tv })
+            const theComment = tv.comments.id(commId)
+            if (req.session.loggedIn) {
+                if (theComment.author == req.session.userId) {
+                    theComment.remove()
+                    tv.save()
+                    res.sendStatus(204)
+                } else {
+                    res.sendStatus(401) // unauthorized
+                }
+            } else {
+                res.sendStatus(401) // unauthorized
+            }
         })
         .catch(err => {
             // catch any errors
             console.log(err)
-            res.status(400).json(err)
+            res.status(400)
         })
 })
 
--- utils/connection.js
+++ utils/connection.js
@@ -21,7 +21,7 @@ mongoose.connect(DATABASE_URL, CONFIG)
 mongoose.connection
     .on('open', () => console.log('Connected to Mongoose'))
     .on('close', () => console.log('Disconnected from Mongoose'))
-    .on('open', (error) => console.log(error))
+    .on('error', (error) => console.log(error))
 
 //////////////////////////////////
 //// Export our Connection    ////

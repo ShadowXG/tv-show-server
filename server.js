147dd0db3a5ed21a39eea7cd75b180560f84d51a
Travis <travishersh0@gmail.com>
2023-01-22 18:00:49 -0500

    update route added

--- server.js
+++ server.js
@@ -84,6 +84,35 @@ app.get('/tv', (req, res) => {
         .catch(err => console.log('The following error occurred: \n', err))
 })
 
+// CREATE route
+// Create -> receives a request, and creates a new document
+app.post('/tv', (req, res) => {
+    const newTvShow = req.body
+    TV.create(newTvShow)
+        .then(tv => {
+            // sending a 201 status, along with the new tv show
+            res.status(201).json({ tv: tv.toObject() })
+        })
+        // send an err if one occurs
+        .catch(err => console.log('The following error occurred: \n', err))
+})
+
+// PUT route
+// Update -> updates a specific tv show
+app.put('/tv/:id', (req, res) => {
+    // save the id for easy reference later
+    const id = req.params.id
+    // save the req.body for easy reference later
+    const updatedTvShow = req.body
+    // find the tv show by id and update it
+    TV.findByIdAndUpdate(id, updatedTvShow, { new: true })
+        .then(tv => {
+            console.log('The updated tv show: ', tv)
+            res.sendStatus(204)
+        })
+        .catch(err => console.log('The following error occurred: \n', err))
+})
+
 ///////////////////////////
 //// Server Listener   ////
 ///////////////////////////

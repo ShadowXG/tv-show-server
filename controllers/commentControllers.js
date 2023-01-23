0cb062a66a08cdb8cc572e34b39a397238eb7306
Travis <travishersh0@gmail.com>
2023-01-23 11:30:40 -0500

    v4 complete: users can now add their own comments

--- controllers/commentControllers.js
+++ controllers/commentControllers.js
@@ -2,7 +2,7 @@
 //// Import Dependencies   ////
 ///////////////////////////////
 const express = require('express')
-const TV = require('../models/tv-shows')
+const Show = require('../models/tv-shows')
 
 //////////////////////////
 //// Create Router    ////
@@ -15,45 +15,48 @@ const router = express.Router()
 
 // POST route
 // only logged in users can post comments
-router.post('/:tvId', (req,res) => {
-    const tvId = req.params.tvId
+router.post('/:showId', (req,res) => {
+    const showId = req.params.showId
     if (req.session.loggedIn) {
         req.body.author = req.session.userId
         const theComment = req.body
-        // find a specific tv show
-        TV.findById(tvId)
-            .then(tv => {
-                tv.comments.push(theComment)
-                // save the tv
-                return tv.save()
+        // find a specific show show
+        Show.findById(showId)
+            .then(show => {
+                show.comments.push(theComment)
+                // save the show
+                return show.save()
             })
-            .then(tv => {
-                // respond with a 201 and the tv itself
-                res.status(201).json({ tv: tv })
+            .then(show => {
+                // respond with a 201 and the show itself
+                // res.status(201).json({ show: show })
+                res.redirect(`/shows/${show.id}`)
             })
             .catch(err => {
                 // catch any errors
                 console.log(err)
-                res.status(400).json(err)
+                // res.status(400).json(err)
+                res.redirect(`/error?error=${err}`)
             })
     } else {
-        res.sendStatus(401)
+        // res.sendStatus(401)
+        res.redirect(`/error?error=You%20Are%20not%20allowed%20to%20comment%20on%20this%20show`)
     }
 })
 
 // DELETE route
 // only the author of the comment can delete the comment
-router.delete('/delete/:tvId/:commId', (req, res) => {
+router.delete('/delete/:showId/:commId', (req, res) => {
     // isolate the id and save them to variables for easy access
-    const { tvId, commId } = req.params
-    // get the tv show
-    TV.findById(tvId)
-        .then(tv => {
-            const theComment = tv.comments.id(commId)
+    const { showId, commId } = req.params
+    // get the show show
+    Show.findById(showId)
+        .then(show => {
+            const theComment = show.comments.id(commId)
             if (req.session.loggedIn) {
                 if (theComment.author == req.session.userId) {
                     theComment.remove()
-                    tv.save()
+                    show.save()
                     // res.sendStatus(204)
                     res.redirect(`/shows/${show.id}`)
                 } else {
